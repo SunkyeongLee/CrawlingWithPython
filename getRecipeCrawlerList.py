ad8b1614876921da312de8d6876cc140bea11964
Sunkyeong Lee <sunkyong9768@gmail.com>
2020-05-19 17:34:53 +0900

    리스트 자동 저장 추가

--- getRecipeCrawlerList.py
+++ getRecipeCrawlerList.py
@@ -2,9 +2,8 @@ import time
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
-from random import randint
 
-class getRecipeCrawlerRand:
+class getRecipeCrawler:
     def __init__(self):
         self.url = 'https://www.haemukja.com/'
         self.nextPage = [
@@ -14,13 +13,12 @@ class getRecipeCrawlerRand:
             '//*[@id="content"]/section/div[2]/div/div[2]/a[7]'
             ]
         self.location = 'a.call_recipe > strong'
-        self.ingKey = ['감자', '양파', '옥수수', '마늘', '식빵']
 
     def launch_crawler(self):
         self.driver = webdriver.Chrome(ChromeDriverManager().install())
         self.driver.get(self.url)
 
-    def find_click(self, xpath, sleep_interval=3):
+    def find_click(self, xpath, sleep_interval=1):
         btn = self.driver.find_element_by_xpath(xpath)
         btn.click()
         time.sleep(sleep_interval)
@@ -35,20 +33,35 @@ class getRecipeCrawlerRand:
         self.soup = BeautifulSoup(html, 'html.parser')
         recipeName = self.soup.select(element)
         for recipe in recipeName:
-            print(recipe.text)
+            return recipe.text
 
     def run(self):
+        result = []
         self.launch_crawler()
         
-        key = self.ingKey[randint(0, len(self.ingKey)-1)]
+        key = input("재료를 입력하세요: ")
         self.find_recipe(key)
 
         for i in range(len(self.nextPage)):
-            self.get_recipe(self.location)
+            recipe = self.get_recipe(self.location)
+
+            recipe_obj = {
+                'recipe_ID': i,
+                'recipe_Name': recipe,
+                'ingredient_Key': key
+            }
+            print(recipe)
+            result.append(recipe_obj)
+
+            self.find_click(self.nextPage[i])
+
+        return print(result)
         
+        
+
         time.sleep(2)
         self.driver.quit()
 
 if __name__ == "__main__":
-    crawler = getRecipeCrawlerRand()
+    crawler = getRecipeCrawler()
     crawler.run()
